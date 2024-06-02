6247b0a28cd860615c199bd4cd3f116bf99fb7ee
PrasadPawar2729 <prasadpawarpp2002@gmail.com>
2024-06-02 20:38:35 +0530

    register login get done

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,7 +1,29 @@
 const express=require("express")
 const bcrypt=require("bcrypt");
+const jwt = require("jsonwebtoken")
 const { userModel } = require("../model/usermodel");
 const userRouter=express.Router()
+
+
+
+//get the user
+userRouter.get("/user",async(req,res)=>{
+    try{
+
+     const user = await userModel.find()
+     res.status(200).json({users:user})
+
+
+    }
+    catch(err){
+        res.status(500).send(err)
+    }
+})
+
+
+
+
+
 
 //register the user
 userRouter.post("/register",async(req,res)=>{
@@ -36,5 +58,50 @@ userRouter.post("/register",async(req,res)=>{
         console.log(err)
     }
 })
+
+
+//login the user 
+
+userRouter.post("/login",async(req,res)=>{
+
+const {email,pass}=req.body;
+
+    try{
+       //check if the user is registred or not
+       const user=await userModel.findOne({email})
+       if(!user){
+           res.status(404).send("user not found")
+       }
+       //check pass is match or not 
+       //if pass is correct send the token
+       
+      bcrypt.compare(pass,user.pass,(err,result)=>{
+        if(err){
+            res.status(500).send(err)
+        }
+        const token = jwt.sign({userID:user._id},"prasad")
+       
+        res.status(200).json({msg:"login successfully",token})
+    
+    })
+
+       
+    }
+    catch(err){
+        res.status(500).send(err)
+    }
+})
+
+
+
+
+
+
+
+
+
+
+
+
 
 module.exports={userRouter};
