7642dfebfc4cd6f50b09967699541a00a5c9fb6f
PrasadPawar2729 <prasadpawarpp2002@gmail.com>
2024-06-02 20:06:56 +0530

    register

--- config/db.js
+++ config/db.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose")
 
 
+//const connection = mongoose.connect("mongodb+srv://prasadpawarpp2002:kanban@cluster0.y6nijti.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 const connection = mongoose.connect("mongodb+srv://prasadpawarpp2002:kanban@cluster0.y6nijti.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 
 module.exports = {connection}
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,12 +1,36 @@
 const express=require("express")
-
+const bcrypt=require("bcrypt");
+const { userModel } = require("../model/usermodel");
 const userRouter=express.Router()
 
-
-userRouter.get("/",async(req,res)=>{
+//register the user
+userRouter.post("/register",async(req,res)=>{
+   
+  const {username,email,pass,role}=req.body; 
     try{
 
-        res.send("hello bhai")
+      //logic for converted pass to hash
+     bcrypt.hash(pass,5,async(err,hash)=>{
+
+        if(err){
+            res.status(500).sendDate(err)
+        }
+         else{
+            const user = new userModel({
+                username,
+                email,
+                pass:hash,
+                role
+            })
+
+           await user.save() 
+            res.status(200).send("user created ")
+         }
+
+     })
+
+
+
     }
     catch(err){
         console.log(err)
